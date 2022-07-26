4baa387b1fa4322e2980fc2518845375a3826d47
MrGians <gianlumura97@gmail.com>
2022-07-26 16:21:31 +0200

    Header Done

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <header>
-    <div id="navbar"></div>
+    <div id="navbar">
+      <div class="container-lg">
+        <img src="./../assets/img/logo.svg" alt="Spotify Logo" />
+      </div>
+    </div>
   </header>
 </template>
 
@@ -10,4 +14,19 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "./../assets/scss/style.scss";
+header {
+  height: 50px;
+  padding: 0.5rem;
+  background-color: $primary_color;
+
+  #navbar {
+    img {
+      width: 100px;
+      height: 100%;
+      filter: invert(66%) sepia(61%) saturate(632%) hue-rotate(85deg) brightness(94%) contrast(86%);
+    }
+  }
+}
+</style>
