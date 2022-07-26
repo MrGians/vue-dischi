95221ebf94e89ba9e92077a6b694ac683eabc4e0
MrGians <gianlumura97@gmail.com>
2022-07-26 17:26:47 +0200

    Added DiscCard component

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -4,14 +4,7 @@
       <div class="container">
         <div class="row py-5">
           <div v-for="disc in discsList" :key="disc.title" class="col py-3">
-            <!-- TODO Component Card -->
-            <div class="disc-card">
-              <img :src="disc.poster" :alt="disc.title" />
-              <h6 class="disc-name">{{ disc.title }}</h6>
-              <div class="artist">{{ disc.author }}</div>
-              <div class="date">{{ disc.year }}</div>
-            </div>
-            <!-- TODO -->
+            <DiscCard :disc="disc" />
           </div>
         </div>
       </div>
@@ -20,8 +13,10 @@
 </template>
 
 <script>
+import DiscCard from "./DiscCard.vue";
 export default {
   name: "BaseMain",
+  components: { DiscCard },
   props: {
     discsList: Array,
   },
@@ -38,28 +33,6 @@ export default {
     .col {
       flex-basis: 20%;
       flex-grow: 0;
-
-      .disc-card {
-        height: 100%;
-        text-align: center;
-        background-color: $secondary_color;
-        padding: 0.8rem;
-
-        .disc-name {
-          text-transform: uppercase;
-          padding-top: 1rem;
-          color: white;
-        }
-        .artist,
-        .date {
-          font-size: 0.8rem;
-          font-weight: 500;
-          color: rgb(119, 119, 119);
-        }
-        .date {
-          font-size: 0.7rem;
-        }
-      }
     }
   }
 }
---
+++ src/components/DiscCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="disc-card">
+    <img :src="disc.poster" :alt="disc.title" />
+    <h6 class="disc-name">{{ disc.title }}</h6>
+    <div class="artist">{{ disc.author }}</div>
+    <div class="date">{{ disc.year }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DiscCard",
+  props: {
+    disc: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../assets/scss/style.scss";
+
+.disc-card {
+  height: 100%;
+  text-align: center;
+  background-color: $secondary_color;
+  padding: 0.8rem;
+
+  .disc-name {
+    text-transform: uppercase;
+    padding-top: 1rem;
+    color: white;
+  }
+  .artist,
+  .date {
+    font-size: 0.8rem;
+    font-weight: 500;
+    color: rgb(119, 119, 119);
+  }
+  .date {
+    font-size: 0.7rem;
+  }
+}
+</style>
